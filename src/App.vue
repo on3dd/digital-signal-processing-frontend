6b1ed94415fe3f1850249354f8a7eb1cb30ad842
Artyom <onedeadwave@gmail.com>
2020-03-04 00:03:12 +1000

    Added router and basic views

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-  <div id="app">
-    <h1>Digital Signal Processing</h1>
-    <ChartComponent id="chart" parent="chart-placeholder"/>
-    <ChartNavigation id="navigation" parent="navigation-placeholder"/>
-    <section class="chart-legend">
-      <span>Chart legend</span>
-    </section>
+  <div>
+    <header id="nav">
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">Файл</router-link>
+          </li>
+          <li>
+            <router-link to="/">Моделирование</router-link>
+          </li>
+          <li>
+            <router-link to="/">Фильтрация</router-link>
+          </li>
+          <li>
+            <router-link to="/">Анализ</router-link>
+          </li>
+          <li>
+            <router-link to="/">Настройки</router-link>
+          </li>
+          <li>
+            <router-link to="/">Справка</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <router-view/>
+    </main>
   </div>
 </template>
-
-<script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import ChartComponent from "@/components/ChartComponent.vue";
-import ChartNavigation from "@/components/ChartNavigation.vue";
-
-@Component({
-  components: {
-    ChartComponent,
-    ChartNavigation
-  },
-})
-export default class App extends Vue {}
-</script>
 
 <style lang="scss">
   @import "./scss/main";
@@ -30,21 +37,52 @@ export default class App extends Vue {}
     display: flex;
     flex-direction: column;
     align-items: center;
-
     padding: 3rem;
-    min-height: 100vh;
+    /*min-height: 100vh;*/
 
     > h1 {
       text-align: center;
     }
 
+    main {
+      width: 100%;
+    }
     section {
       margin: .5rem;
       width: 80%;
-
       &.chart-legend {
         padding: 1rem;
         border: 1px solid #dddddd;
+      }
+    }
+  }
+
+  header {
+    width: inherit;
+    overflow-x: hidden;
+
+    > nav {
+      width: inherit;
+      background-color: #fafafa;
+
+      > ul {
+        display: flex;
+        padding: 1rem;
+
+        li {
+          font-size: inherit;
+          padding: .5rem 1rem;
+          transition: background-color .1s ease-out;
+
+          a {
+            text-decoration: none;
+            color: inherit;
+          }
+
+          &:hover {
+            background: rgba(0, 0, 0, .1);
+          }
+        }
       }
     }
   }
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Digital Signal Processing</h1>
+    <ChartComponent id="chart" parent="chart-placeholder"/>
+    <ChartNavigation id="navigation" parent="navigation-placeholder"/>
+    <section class="chart-legend">
+      <span>Chart legend</span>
+    </section>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import ChartComponent from "@/components/ChartComponent";
+  import ChartNavigation from "@/components/ChartNavigation";
+
+  export default {
+    name: 'Home',
+    components: {
+      ChartComponent,
+      ChartNavigation
+    }
+  }
+</script>
