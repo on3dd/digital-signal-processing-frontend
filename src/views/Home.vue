f3c2e7d9df64c6de7d40c9cb40aca16726bd0000
Artyom <onedeadwave@gmail.com>
2020-03-08 17:14:40 +1000

    Minor changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
-  <div id="app">
+  <div class="home">
     <h1>Digital Signal Processing</h1>
     <TelegramChart :options="options"/>
   </div>
 </template>
 
 <script lang="ts">
-  // @ is an alias to /src
-  // import ChartComponent from "@/components/ChartComponent";
-  // import ChartNavigation from "@/components/ChartNavigation";
-  import {Component, Vue} from 'vue-property-decorator';
   import TelegramChart from "@/components/TelegramChart.vue";
   import {transformData} from '@/utils';
   import themes from '@/themes';
@@ -19,26 +15,14 @@
   export default {
     name: 'Home',
     components: {
-      // TelegramChart: {
-      //   props: {
-      //     options: {
-      //       // el: document.getElementById('chart1'),
-      //       width: 600,
-      //       height: 200,
-      //       data: transformData(data[0] as DataItem),
-      //       theme: themes.day,
-      //       animationSpeed: 20
-      //     }
-      //   }
-      // }
       TelegramChart,
     },
     data() {
       return {
         options: {
           // el: document.getElementById('chart1'),
-          width: 600,
-          height: 200,
+          width: 800,
+          height: 300,
           data: transformData(data[0] as DataItem),
           theme: themes.day,
           animationSpeed: 20
@@ -47,3 +31,17 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3rem;
+    width: 100%;
+
+    > h1 {
+      text-align: center;
+    }
+  }
+</style>
